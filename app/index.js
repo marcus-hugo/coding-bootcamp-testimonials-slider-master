026bd8cebdd9acadc277f1e107f174583e81899d
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2022-02-05 18:04:50 -0600

    changed button order

--- app/index.js
+++ app/index.js
@@ -5,11 +5,11 @@ const slideOne = document.querySelector('.slide--one');
 const slideTwo = document.querySelector('.slide--two');
 
 previous.addEventListener('click', function() {
-    slideOne.style.display = 'none';
-    slideTwo.style.display = 'block';
+    slideOne.style.display = 'block';
+    slideTwo.style.display = 'none';
 })
 
 next.addEventListener('click', function() {
-    slideOne.style.display = 'block';
-    slideTwo.style.display = 'none';
+    slideOne.style.display = 'none';
+    slideTwo.style.display = 'block';
 })
