01d4d7951ea72b21c39f5028c295fb363a922d38
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2022-04-02 17:46:41 -0500

    updates functionality

--- app/index.js
+++ app/index.js
@@ -1,17 +1,29 @@
-// buttons
 const previous = document.querySelector('.slide-control--previous');
 const next = document.querySelector('.slide-control--next');
+const slides = document.querySelectorAll('.slide');
 
-// slides
-const slideOne = document.querySelector('.slide--one');
-const slideTwo = document.querySelector('.slide--two');
+let slideIndex = 1;
 
-previous.addEventListener('click', function() {
-    slideOne.classList.toggle('current');
-    slideTwo.classList.toggle('next');
+previous.addEventListener('click', function () {
+    nextSlide(-1);
+})
+next.addEventListener('click', function () {
+    nextSlide(1);
 })
 
-next.addEventListener('click', function() {
-    slideOne.classList.toggle('current');
-    slideTwo.classList.toggle('next');
-})
+function nextSlide(n) {
+    currentSlide(slideIndex += n);
+}
+
+function currentSlide(n) {
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "flex";
+}
