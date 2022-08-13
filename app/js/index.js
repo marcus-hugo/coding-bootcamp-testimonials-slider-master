51120b38628f11f8b7778b38569934d4c70ecf70
marcus-hugo <78324205+marcus-hugo@users.noreply.github.com>
2022-08-13 12:58:29 -0500

    added folders for js and scss and fixed layouts

--- app/js/index.js
+++ app/js/index.js
@@ -22,7 +22,7 @@ function currentSlide(n) {
     if (n < 1) {
         slideIndex = slides.length;
     }
-    for (i = 0; i < slides.length; i++) {
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "flex";
